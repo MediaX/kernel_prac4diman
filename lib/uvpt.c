75a74b1538d3d602c30a6585264c652c2de14a7d
Dimitrii Matevosov <xmdimitrii@yandex.ru>
2023-01-07 11:26:51 +0300

    Lab 12 fix.

--- lib/uvpt.c
+++ lib/uvpt.c
@@ -42,6 +42,14 @@ int
 foreach_shared_region(int (*fun)(void *start, void *end, void *arg), void *arg) {
     /* Calls fun() for every shared region */
     // LAB 11: Your code here
+    // for (size_t i = 0; i < MAX_USER_ADDRESS; i+=PAGE_SIZE){
+    //     if ((get_uvpt_entry((void *)i) & (PTE_P | PTE_SHARE)) == ((PTE_P | PTE_SHARE))){
+    //         int r = fun((void *)i, (void *)(i + PAGE_SIZE), arg);
+    //         if (r < 0)
+    //             panic("foreach_shared_region: fun %i", r);
+    //     }
+    // }
+
     for (uintptr_t i = 0; i < MAX_USER_ADDRESS; i += (1LL << PML4_SHIFT)){
         if (uvpml4[VPML4(i)] & PTE_P){
             for (uintptr_t j = i; j < i + (1LL << PML4_SHIFT); j += (1LL << PDP_SHIFT)){
